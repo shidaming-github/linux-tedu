fd8cff7f70e5203f684d90537af3aa1cb7556599
石大明 <55618944+shidaming-github@users.noreply.github.com>
2019-11-20 00:34:25 -0600

    Delete NFSfw.sh

--- NFSfw.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rpm -q nfs-utils &> /dev/null
-[ $? -gt 0 ]&& yum -y install nfs-utils
-mkdir /public
-echo "/public    *(ro)" > /etc/exports
-systemctl restart nfs-server
-systemctl enable nfs-server
-#firewall-cmd --set-default-zone=trusted &> /dev/null
